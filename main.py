16d2b52aafe3aa2270954f028bdffa420f669b60
Otavio <tavi.solli2018@gmail.com>
2026-08-19 08:18:12 -0300

    Adiciona a Rota sobre

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ def help():
 def users():
     return {"nome": "Otavio", "email": "tavi123@gmail"}
 
+@app.route("/sobre")
+def sobre():
+    return "Api direcionada aos usuarios"
+
 if __name__ == "__main__":
    app.run(debug=True)
